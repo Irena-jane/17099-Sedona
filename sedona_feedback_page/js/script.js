e7c1bc591eb91340527057b9c4ad9bca03bfae2e
Irena <eirnvn@mail.ru>
2015-03-25 00:43:03 +0400

    javascript второе задание 1 часть

--- sedona_feedback_page/js/script.js
+++ sedona_feedback_page/js/script.js
@@ -5,9 +5,10 @@
 
 (function() {
   var wdg_counters = document.querySelectorAll('.js-counter');
-  // var input_tmpl = document.querySelector('#input-template').innerHTML;
-  // var adults = ['взрослого'];
-  // var children = ['ребенка'];
+  var input_tmpl = document.querySelector('#input-template').innerHTML;
+  var clients = {};
+  clients.adults = ['взрослого', 1];
+  clients.children = ['ребенка', 0];
 
   var min_counter = 0;
   var max_counter = 30;
@@ -24,22 +25,26 @@
   function changeCounter(e){
     var e = e || window.event;
     var target = e.target;
-
+    e.preventDefault();
     if(target.tagName != 'BUTTON') return;
 
     var input = this.querySelector('input');
     var min = input.dataset.min || min_counter;
     var max = input.dataset.max || max_counter;
+    var val = parseInt(input.value);
 
      if(target.dataset.count == 'minus') {
-      if(--input.value < min) input.value = min;
-
+      val -=1;
+      if(val < min) val = min;
      }
+
      if(target.dataset.count == 'plus') {
-      max != 'unlimit' && input.value > (max-1)? input.value = max : ++input.value;
-
+        val +=1;
+        if(max != 'unlimit' && val > max) val = max;
      }
-    return false;
+
+     input.value = val;
+     operateClientInput(target, val);
   };
 
   function checkCounter(e){
@@ -48,7 +53,10 @@
     var min = target.dataset.min || min_counter;
     var max = target.dataset.max || max_counter;
     var val = checkValue(target.value, min, max);
+
     target.value = val;
+    operateClientInput(target, val);
+
   };
 
   function checkValue(val, min, max){
@@ -60,7 +68,43 @@
     return res;
   };
 
-    // function generateCustomerInputs(val){
-    //   // var row =
-    // };
+    function operateClientInput(target, val){
+      var client_type = target.parentNode.dataset.info;
+      var number = clients[client_type][1];
+
+      if(val - number > 0) {
+        addClientInput(client_type, val, number);
+      }else{
+        removeClientInput(client_type, val, number);
+      }
+
+
+    };
+    function addClientInput(client_type, val, number){
+      var output = document.querySelector('.js-output-'+client_type);
+      var type = clients[client_type][0];
+
+      for(var i= number; i < val;i++,++clients[client_type][1]){
+        var row = document.createElement('div');
+        row.classList.add('row');
+        var html = Mustache.render(input_tmpl, {
+          "type": type,
+          "number": i+1
+        });
+        row.innerHTML = html;
+        output.appendChild(row);
+      }
+
+    };
+
+
+    function removeClientInput(client_type, val, number){
+      var output = document.querySelector('.js-output-'+client_type);
+
+      for(var i = 0, len = number - val; i < len; i++){
+        output.removeChild(output.lastChild);
+        clients[client_type][1]-=1;
+      }
+
+    };
 })();
